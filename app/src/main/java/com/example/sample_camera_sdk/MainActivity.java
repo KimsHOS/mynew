b5483d9078ac1afffa42dd0425ddc247415cd571
Moses Burla <moses.burla@achalasolutions.com>
2025-10-21 14:11:15 +0530

    latest

--- app/src/main/java/com/example/sample_camera_sdk/MainActivity.java
+++ app/src/main/java/com/example/sample_camera_sdk/MainActivity.java
@@ -30,40 +30,12 @@ public class MainActivity extends AppCompatActivity {
             List<String> achalaActions = new ArrayList<>();
 //            achalaActions.add(AchalaActions.Blink);
 //            achalaActions.add(AchalaActions.Smile);
-            //achalaActions.add(AchalaActions.Open_Eyes);
+            achalaActions.add(AchalaActions.Open_Eyes);
             achalaSecure.setActions(achalaActions);
 
-//            achalaSecure.liveNessDetection(100, "123");
             achalaSecure.liveNessDetection(100, "123");
 //            Intent intent = new Intent(this, CameraActivity.class);
 //            startActivity(intent);
         });
     }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 100) { // The same request code you passed in liveNessDetection
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    String message = data.getStringExtra("resultMessage");
-                    boolean success = data.getBooleanExtra("success", false);
-
-                    // Handle the success
-                    if (success) {
-                        // Do something with the result
-                        System.out.println("Liveness check passed: " + message);
-                    } else {
-                        System.out.println("Liveness check failed: " + message);
-                    }
-                }
-            } else if (resultCode == RESULT_CANCELED) {
-                System.out.println("Liveness check canceled by user.");
-            }
-        }
-    }
-
 }
-
